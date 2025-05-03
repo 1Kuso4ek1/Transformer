8d43a5b908eb505b57548f5fedad2f083003725b
1Kuso4ek1 <dimasalov2006@gmail.com>
2025-05-03 18:47:21 +0300

    Huh, I'm sorry for this garbage... Seems like I was too lazy xd

--- include/Inferencer.hpp
+++ include/Inferencer.hpp
@@ -9,17 +9,24 @@
 class Inferencer {
 public:
     Inferencer(const Tokenizer& tokenizer, std::shared_ptr<Transformer> transformer, size_t maxSize)
-        : tokenizer(tokenizer), transformer(transformer), maxSize(maxSize), temperature(0.5) {}
+        : tokenizer(tokenizer), transformer(transformer), maxSize(maxSize), temperature(0.5)
+    {}
 
-    void run() {
+    void run()
+    {
         printInstructions();
         
-        while(true) {
+        while(true)
+        {
             std::print("> ");
             std::getline(std::cin, userInput);
             
-            if(!handleCommands())
+            auto lastCommand = handleCommands();
+            
+            if(lastCommand == "exit")
                 break;
+            else if(!lastCommand.empty())
+                continue;
 
             processInput();
             generateResponse();
@@ -27,41 +34,50 @@ public:
     }
 
 private:
-    void printInstructions() {
+    void printInstructions()
+    {
         std::println("Inference mode. Type 'exit' to quit.");
         std::println("Type 'reset' to clear context.");
         std::println("Type 'temperature <value>' to set temperature.");
         std::println("Type 'pass' to skip the input.\n");
     }
 
-    bool handleCommands() {
+    std::string handleCommands()
+    {
         if(this->userInput == "exit")
-            return false;
+            return "exit";
 
-        if(this->userInput == "reset") {
+        if(this->userInput == "reset")
+        {
             this->context.clear();
             std::println("Context cleared.\n");
-            return true;
+
+            return "reset";
         }
         
         auto pos = this->userInput.find("temperature");
-        if(pos != std::string::npos) {
+        if(pos != std::string::npos)
+        {
             this->temperature = std::stof(this->userInput.substr(pos + 12));
             std::println("Temperature set to {}.\n", this->temperature);
-            return true;
+
+            return "temperature";
         }
 
-        return true;
+        return "";
     }
 
-    void processInput() {
-        if(this->userInput != "pass") {
+    void processInput()
+    {
+        if(this->userInput != "pass")
+        {
             this->context += " [USER] " + this->userInput;
             this->context += " [ASSISTANT] ";
         }
     }
 
-    void generateResponse() {
+    void generateResponse()
+    {
         auto tokens = this->tokenizer.encode(this->context);
         
         if(tokens.size() > this->maxSize)
@@ -70,20 +86,26 @@ private:
         auto output = this->transformer->generate(std::move(tokens), this->maxSize, 128, this->temperature);
         
         std::print("\nPredicted: ");
-        for(const auto& i : output) {
-            if(i != 0 && i != 2) {
+        for(const auto& i : output)
+        {
+            if(i != 0 && i != 2)
+            {
                 auto token = this->tokenizer.decode(i);
                 std::print("{} ", token);
                 this->context += token + ' ';
             }
         }
+
         std::println("\n");
     }
 
+private:
     const Tokenizer& tokenizer;
     std::shared_ptr<Transformer> transformer;
+    
     size_t maxSize;
     float temperature;
+
     std::string context;
     std::string userInput;
 };
